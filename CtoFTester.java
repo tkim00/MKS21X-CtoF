09106e60ec00dc52f0ab2bc47d872792db9df0a1
Taejoon Kim <tkim00@stuy.edu>
2018-09-20 16:40:41 -0400

    not done yet

---
+++ CtoFTester.java
@@ -0,0 +1,14 @@
+public class CtoFTester{
+
+public static void main(String[] args){
+}
+
+	public static int celsiusToFahrenheit(double c){
+		return c * (9/5) + 32;
+	}
+	
+	public static int fahrenheitTocelsius(double f){
+		return (f - 32) * (5/9);
+	}	
+
+}
